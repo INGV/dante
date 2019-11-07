bbb402a511961477897d0ca01c4be5a15ae73206
Valentino Lauciani <valentino.lauciani@ingv.it>
2019-11-07 14:11:28 +0100

    update

--- routes/api.php
+++ routes/api.php
@@ -20,15 +20,14 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 /* START - EventDB API */
 Route::prefix('eventdb/_table/v1')->group(function(){
     Route::apiResources([
-        'event'             => 'App\Api\v1\Controllers\EventController',
         'hypocenter'        => 'App\Api\v1\Controllers\HypocenterController',
         'magnitude'         => 'App\Api\v1\Controllers\MagnitudeController',
         'type_event'         => 'App\Api\v1\Controllers\TypeEventController',
         'amplitude'         => 'App\Api\v1\Controllers\AmplitudeController',
+        'event'         => 'App\Api\v1\Controllers\EventController',
         // --PLACEHOLDER-- - Used from 'ingv/script.sh'; DO NOT REMOVE!!!
         ],[
             'parameters' => [
-                'event' => 'id',
                 ],
             'middleware' => [
                 'App\Api\Middleware\JsonApiMiddleware'
