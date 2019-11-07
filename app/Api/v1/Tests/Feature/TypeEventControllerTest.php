b202d990dcc45bd6badf7af4295d281a7f42e809
Valentino Lauciani <valentino.lauciani@ingv.it>
2019-11-07 11:41:47 +0100

    Add 'type_event' route

---
+++ app/Api/v1/Tests/Feature/TypeEventControllerTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Api\v1\Tests\Feature;
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+use App\Api\v1\Tests\DanteBaseTest;
+
+class TypeEventControllerTest extends DanteBaseTest
+{
+    protected $uri = '/api/eventdb/_table/v1/type_event';
+    /* 
+     * Do not insert all fields that are auto generated; for example:
+     *  - 'id' (that is autoincremente) 
+     *  - 'inserted' (that is auto-generated)
+     *  - 'modified' (that is auto-generated) 
+     */
+    protected $inputParameters = [
+        'name'          => 'earthquakeTest55',
+        'short'         => null,
+        'description'   => null
+    ];   
+    protected $inputParametersForUpdate = [
+		'description'   => 'earthquakeTest56'
+    ];
+    protected $data = [
+        'id',
+        'name',
+        'short',
+        'description',
+        'modified',
+        'inserted'
+    ]; 
+}
